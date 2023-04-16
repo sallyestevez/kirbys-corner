cf4ca201b1640fde35997512b9ef548c13b1a6f3
Sally <sme418@nyu.edu>
2023-04-16 01:44:56 -0400

    Fixed character page layout

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-new Glide(".glide").mount();
+const config = {
+  type: 'carousel',
+  perView: 3,
+  breakpoints: {
+    1024: {
+      perView: 2
+    },
+    600: {
+      perView: 1
+    }
+  }
+}
+
+new Glide(".glide", config).mount();
 
 function openAbility(evt, abilityName) {
   // Declare all variables
